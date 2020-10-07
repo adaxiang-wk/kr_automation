b39f76cc202b567c906b9480b487fddd69471cc1
Ada Xiang <ada.xiang@dealogic.com>
2020-10-07 18:32:07 +0800

    cleaned issuer names

--- infoParser/data_utils.py
+++ infoParser/data_utils.py
@@ -1,8 +1,25 @@
 import pandas as pd 
 import json
+import re
+import ast
+
+
+def _clean_bkr(bkr):
+    bkr = re.sub(r'\n+', '', bkr)
+    if bkr == '[]' or bkr == "['[]']":
+        cleaned = '[]'
+    else:
+        bkr = ast.literal_eval(bkr)
+        cleaned = list(map(lambda x: re.sub(r"㈜|\n+|[\(\[].*?[\)\]]", "", x), bkr))
+    
+    print(cleaned)
+    return cleaned
+
 
 def load_bkr_df(fp):
     bkr_df = pd.read_csv(fp)
+    bkr_df['발행기관명'] = bkr_df['발행기관명'].apply(lambda x: re.sub(r'[\(\[].*?[\)\]]', "", x))
+    bkr_df['bookrunners'] = bkr_df['bookrunners'].apply(lambda x: _clean_bkr(x))
     
     with open('./dependencies/kr_eng_translation.json', encoding='UTF-8-sig') as js_file:
         translation = json.load(js_file)
@@ -11,4 +28,6 @@ def load_bkr_df(fp):
 
     translated_df.to_csv('./data/translated_bkr_df.csv')
 
-    return translated_df
+    return translated_df 
+
+
--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import infoParser.data_utils as du
+import re
+import ast
 
 if __name__ == "__main__":
     bkr_fp = './data/bkr_df.csv'
     du.load_bkr_df(bkr_fp)
+
+    # bkrs = u"['NH투자증권㈜', '한국투자증권㈜', 'KB증권㈜']"
+    # bkrs = ast.literal_eval(bkrs)
+    # print(list(map(lambda x: re.sub(r"㈜|\n+|[\(\[].*?[\)\]]", "", x), bkrs)))
